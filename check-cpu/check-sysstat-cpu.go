26619d5db2a05bc5ffa07cdaaedfbe8d1c7a2780
inokappa <inokara@gmail.com>
2014-12-29 16:15:49 +0900

    add check-sysstat-cpu.go

--- check-cpu/check-sysstat-cpu.go
+++ check-cpu/check-sysstat-cpu.go
@@ -1,19 +1,49 @@
 package main
 
 import (
-    "bitbucket.org/bertimus9/systemstat"
-    "fmt"
-    "time"
+	"bitbucket.org/bertimus9/systemstat"
+	"fmt"
+	"time"
+	"flag"
+	"os"
+	"strconv"
 )
 
 var last_cpu systemstat.CPUSample
 var current_cpu systemstat.CPUSample
-var cpu systemstat.CPUAverage
+var cpu_stat systemstat.SimpleCPUAverage
+var cpu_average systemstat.CPUAverage
 
 func main() {
-    last_cpu = systemstat.GetCPUSample()
-    time.Sleep(1000 * time.Millisecond) 
-    current_cpu = systemstat.GetCPUSample()
-    cpu = systemstat.GetCPUAverage(last_cpu, current_cpu) 
-    fmt.Println(cpu)
+	var w = flag.String("warning", "80", "Warning Level(%)")
+	var c = flag.String("critical", "100", "Critical Level(%)")
+	//var s = flag.Int("Sleep", 1000, "Set SleepTime")
+	flag.Parse()
+
+	warning, _ := strconv.ParseFloat(*w, 64)
+	critical, _ := strconv.ParseFloat(*c, 64)
+	//sleep, _ := strconv.ParseFloat(*s, 64)
+
+	last_cpu = systemstat.GetCPUSample()
+	//fmt.Println(last_cpu)
+	time.Sleep(1000 * time.Millisecond) 
+	current_cpu = systemstat.GetCPUSample()
+	//fmt.Println(current_cpu)
+
+	cpu_stat = systemstat.GetSimpleCPUAverage(last_cpu, current_cpu)
+	cpu_average = systemstat.GetCPUAverage(last_cpu, current_cpu)
+
+	fmt.Printf("CPUAverage=")
+	fmt.Println(cpu_average)
+
+	if cpu_stat.BusyPct > critical {
+		fmt.Println("critical")
+		os.Exit(2)
+	} else if cpu_stat.BusyPct > warning {
+		fmt.Println("warning")
+		os.Exit(1)
+	} else {
+		fmt.Println("ok")
+		os.Exit(0)
+	}
 }
